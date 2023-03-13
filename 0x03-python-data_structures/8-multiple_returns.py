e542be25162dc7390d1dcde8d329a847126b7c25
bashir <lawalmuhdbashir@gmail.com>
2023-03-13 10:20:40 -0700

    I added new tasks

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -5,4 +5,4 @@ def multiple_returns(sentence):
     if len(sentence) == 0:
         return (None)
     else:
-        return (len(sentence), sentence[0])
+        return (len(sentence), sentence[0:1])
